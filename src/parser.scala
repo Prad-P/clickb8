e8ba4823fab0a119b263868001d76c96b0bc78aa
zappymax <westmax@sbcglobal.net>
2017-04-25 18:16:05 -0500

    started work on regex cases

--- src/parser.scala
+++ src/parser.scala
@@ -9,7 +9,27 @@ object parser extends App {
   }
 
   def parse(line:String) : Int = {
-  	println(line.length());
+  	/*val aSplit = line.split(" ")
+  	try{
+  		val temp = aSplit[0].toInt
+  	}
+  	catch {
+  		case _: println("not a number")
+  	}
+  	//println(line.length());*/
+  	//Curent theory is that we will be able to use this regex match=>case to divide up the parsing into various methods that we can use
+  	//to parse the individual vocab
+  	line match {
+  		case s if s.matches("\\A[0-9].") => println("list declaration")
+  		case s if s.matches(".[:].") => println("assignment")
+  		case s if s.matches("\\znext!") => println("endblock")
+  		case _ => println("doesn't match any known line syntax: error")
+  	}
+
   	0
   }
+
+  /*def parseMatch(x:String) : String = x match {
+  	//case ""
+  }*/
 }
